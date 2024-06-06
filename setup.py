a5d69caf26976d70da0fe9e5fb71486349bd4d09
Emile Kroeger <e.kroeger@unitedrobotics.group>
2024-06-06 10:50:01 +0200

    flag as universal, bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='naowidgets',
-    version='0.1.0',
+    version='0.1.1',
     package_dir={'': 'src'},
     py_modules=['naowidgets'],
     install_requires=[
         'ipywidgets',
         'IPython'
     ],
+    platforms="any",
     author='Emile Kroeger',
     author_email='e.kroeger@unitedrobotics.group',
-    description='A small library for NAO widgets',
+    description='A small library with useful widgets for NAO in Jupyter.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/NaosClassroom/naowidgets',
